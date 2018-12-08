d7e7ac6fa33a91ce838e78daf1fc91c5b6b106f1
iepoch <staclem@live.com>
2018-12-08 10:37:19 -0700

    Added actions post, get by id

--- data/routers/actionRouter.js
+++ data/routers/actionRouter.js
@@ -5,7 +5,9 @@ const actions = require("../helpers/actionModel");
 router.get("/", (req, res) => {
   actions
     .get()
-    .then()
+      .then(actions => {
+        res.json(actions)
+    })
     .catch(err => {
     res.status(500).json({ error: "The actions could not be found" });
     });
@@ -13,8 +15,36 @@ router.get("/", (req, res) => {
 
 
 
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+
+    actions
+        .get(id)
+        .then(action => {
+            action ?
+                res.status(200).json(action)
+                :
+                res.status(400).json({error: "The actions does not exist"})
+        })
+        .catch()
 
 
+})
+
+router.post('/', (req, res) => {
+    const { project_id, description, notes } = req.body
+
+    if (project_id, description, notes) {
+        actions.insert(req.body)
+            .then(action => res.status(201).json(action))
+            .catch(err => {
+                res.status(500).json({
+                    error: "Error when adding the action"})
+            })
+    } else {
+        res.status(400).json({error: "Please add notes, description and project id."})
+    }
+})
 
 
 
